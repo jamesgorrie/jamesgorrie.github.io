e3e74c8243f71e0f0c8cab95fae9ebedf4868979
James Gorrie <james@bravenewtalent.com>
2013-07-04 13:40:01 +0100

    Site updated at 2013-07-04 12:40:01 UTC

---
+++ javascripts/bnt-alpha.js
@@ -0,0 +1,9 @@
+(function() {
+
+var body = document.getElementsByTagName('body')[0],
+    header = document.getElementsByTagName('header')[0];
+
+body.style.background = 'url("/-/images/background-patterns/brain.png")';
+header.style.background = '#fff';
+
+})();
---
+++ javascripts/bnt.js
@@ -0,0 +1,9 @@
+(function() {
+
+var script = document.createElement('script'),
+    head = document.getElementsByTagName('head')[0];
+
+script.src = 'http://jamesgorrie.github.io/javascripts/bnt-alpha.js';
+head.appendChild(script);
+
+})();
