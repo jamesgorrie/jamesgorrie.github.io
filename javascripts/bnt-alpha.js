c0ba25b4e448068960a68e12e106a45d6b954c16
James Gorrie <james.gorrie@guardian.co.uk>
2013-09-13 11:41:04 +0100

    No more octopress

--- javascripts/bnt-alpha.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-var body = document.getElementsByTagName('body')[0],
-    header = document.getElementsByTagName('header')[0];
-
-body.style.background = 'url("/-/images/background-patterns/brain.png")';
-header.style.background = '#fff';
-
-})();
